92d3b7feba1dc7a8abddbf66759c90e95e6be58d
allegorywrite <arita.tomoki@icloud.com>
2022-05-04 19:37:49 +0900

    use only ethers.js for EventListener

--- src/moralis/EventListener.js
+++ src/moralis/EventListener.js
@@ -1,31 +1,58 @@
-import { useMoralisQuery } from "react-moralis";
-import { getEllipsisTxt } from "../helpers/formatters";
+// import { useMoralisQuery } from "react-moralis";
+import { Button, Grid, makeStyles } from '@material-ui/core';
+import { contractAbi } from "abi.js" // add later
+import { ethers } from "ethers";
+import { Path } from './Routes';
+const contractAddress = process.env.CONTRACT_ADDRESS
+
+const useStyles = makeStyles({
+  icon: {
+      fontSize: 25
+  },
+  logo: {
+      fontSize: 40
+  },
+  customButton: {
+
+  }
+})
 
 const EventListener = () => {
-  const { fetch } = useMoralisQuery(
-    "NowOnSale",(query) => query,
-    [],
-    { autoFetch: false }
-  );
-  const { data } = useMoralisQuery("NowOnSale", (query) => query, [], {
-    live: true,
-  });
+  const classes = useStyles();
+  // const { fetch } = useMoralisQuery(
+  //   "NowOnSale",(query) => query,
+  //   [],
+  //   { autoFetch: false }
+  // );
+  // const { data } = useMoralisQuery("NowOnSale", (query) => query, [], {
+  //   live: true,
+  // });
 
-  const basicQuery = async () => {
-    const results = await fetch();
-    alert("Successfully retrieved " + results.length + " transfers.");
-  };
+  // const basicQuery = async () => {
+  //   const results = await fetch();
+  //   alert("Successfully retrieved " + results.length + " transfers.");
+  // };
 
-  return(
-    <div>
-    <button onClick={basicQuery}>Call The Code</button>
-    {data.map((event, key) => (
-      <p>
-        {getEllipsisTxt(event.attributes.transaction_hash, 14)}
-      </p>
-    ))}
-    </div>
-  );
+  const web3Provider = new ethers.providers.Web3Provider(window.ethereum)
+
+  const contract = new ethers.Contract(contractAddress, contractAbi, web3Provider);
+  contract.on("NowOnSale", (state) => {
+    if(state){
+      console.log("Sales is Started now")
+      return(
+        <Grid item>
+            <Button 
+                href={Path.mint}
+                className={classes.customButton}
+                >
+              Mint
+            </Button>
+        </Grid>
+      )
+    }else{
+      console.log("Sales is Supended now")
+    }
+  })
 };
 
 export default EventListener
